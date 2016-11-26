747bcd86e3748d80ec6bb80622787e592ccac1d9
Mayur Andulkar <mayurv.andulkar@gmail.com>
2016-11-26 15:34:51 +0100

    First commit

---
+++ random_walks/trials.py
@@ -0,0 +1,20 @@
+'''
+Created on 26 Nov 2016
+
+@author: osboxes
+'''
+
+import random
+
+# Code Sample A
+mylist = []
+
+for i in range(random.randint(1, 10)):
+    random.seed(0)
+    a = random.randint(1, 10)
+    print(a)
+    if  a > 3:
+        number = random.randint(1, 10)
+        if number not in mylist:
+            mylist.append(number)
+print(mylist)
